aa375450be8c8da7597424a201b2c91f3211afc1
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2020-12-26 04:26:16 +0000

    plan is working

--- terraform/1_server_cluster/main.tf
+++ terraform/1_server_cluster/main.tf
@@ -7,10 +7,10 @@ resource "aws_launch_configuration" "example" {
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
   user_data       = <<-EOF
-                #!/bin/bash
-                echo "Hello World!" > index.html
-                nohup busybox hhtpd -f -p "${var.server_port}" &
-              EOF
+                  #!/bin/bash
+                  echo "Hello,World!">index.html
+                  nohup busybox httpd -f -p "${var.server_port}" &
+                  EOF
 
   lifecycle {
     create_before_destroy = true
